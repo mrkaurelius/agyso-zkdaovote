38928b044af5a052101ba396520e35fccede6c66
Abdulhamit Kumru <abdulhamit@kumru.dev>
2024-10-19 20:26:03 +0300

    daovote-rs updates

--- sdk/daovote-rs/src/main.rs
+++ sdk/daovote-rs/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use aligned_sdk::core::errors::SubmitError;
+use aligned_sdk::core::types::Network;
+use aligned_sdk::core::types::{AlignedVerificationData, ProvingSystemId, VerificationData};
+use aligned_sdk::sdk::{get_next_nonce, submit_and_wait_verification};
+
 fn main() {
     println!("Hello, world!");
 }
