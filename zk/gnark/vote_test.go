f37af6ab99673844e9eb0395143c2d757c54eddd
midmotor <alidoganmath@gmail.com>
2024-10-19 13:50:39 +0300

    hariboHamit

--- zk/gnark/vote_test.go
+++ zk/gnark/vote_test.go
@@ -5,6 +5,9 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/frontend/cs/scs"
+	"github.com/consensys/gnark/test/unsafekzg"
 	"math/big"
 	"os"
 	"strings"
@@ -13,9 +16,10 @@ import (
 	"github.com/consensys/gnark-crypto/ecc"
 	bn254 "github.com/consensys/gnark-crypto/ecc/bn254/twistededwards"
 	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/backend/plonk"
 	wit "github.com/consensys/gnark/backend/witness"
+	cs "github.com/consensys/gnark/constraint/bn254"
 	"github.com/consensys/gnark/frontend"
-	"github.com/consensys/gnark/frontend/cs/r1cs"
 )
 
 // run carefully!!
@@ -179,4 +183,74 @@ func TestCircuit(t *testing.T) {
 	}
 
 	t.Log("Serialised proof verification succeeded")
+}
+
+func TestPlonkCircuit(t *testing.T) {
+	//create pair
+	priv, _ := rand.Int(rand.Reader, ecc.BN254.ScalarField())
+	pub := new(bn254.PointAffine).ScalarMultiplication(&Base, priv)
+
+	// weight
+	weight := new(big.Int).SetInt64(10)
+
+	// create current bc Votes
+	votes := []*big.Int{new(big.Int).SetInt64(10), new(big.Int).SetInt64(20), new(big.Int).SetInt64(30), new(big.Int).SetInt64(40)}
+	randoms := []*big.Int{new(big.Int).SetInt64(1111), new(big.Int).SetInt64(2222), new(big.Int).SetInt64(3333), new(big.Int).SetInt64(4444)}
+	currentEncVotes := CreateVotes(votes, randoms, pub)
+
+	// create add Votes
+	addVotes := []*big.Int{new(big.Int).SetInt64(1), new(big.Int).SetInt64(2), new(big.Int).SetInt64(3), new(big.Int).SetInt64(4)}
+	addRandoms := []*big.Int{new(big.Int).SetInt64(111), new(big.Int).SetInt64(222), new(big.Int).SetInt64(333), new(big.Int).SetInt64(444)}
+	addEncVotes := CreateVotes(addVotes, addRandoms, pub)
+
+	// create new stage of Votes
+	newEncVotes := AddVotes(currentEncVotes, addEncVotes)
+	//
+
+	var circuit CircuitMain
+	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &circuit)
+
+	r1cs := ccs.(*cs.SparseR1CS)
+	srs, srsLagrangeInterpolation, err := unsafekzg.NewSRS(r1cs)
+	pk, vk, _ := plonk.Setup(ccs, srs, srsLagrangeInterpolation)
+
+	assignment := CircuitMain{}
+
+	assignment.VoteWeight = weight
+	assignment.MasterPubKey.X = pub.X
+	assignment.MasterPubKey.Y = pub.Y
+
+	for i := 0; i < COUNT; i++ {
+		assignment.Randoms[i] = addRandoms[i]
+		assignment.Vote[i] = addVotes[i]
+
+		assignment.EncVoteNew.ElGamals[i].Left.X = newEncVotes.ElGamals[i].Left.X
+		assignment.EncVoteNew.ElGamals[i].Left.Y = newEncVotes.ElGamals[i].Left.Y
+		assignment.EncVoteNew.ElGamals[i].Right.X = newEncVotes.ElGamals[i].Right.X
+		assignment.EncVoteNew.ElGamals[i].Right.Y = newEncVotes.ElGamals[i].Right.Y
+
+		assignment.EncVoteOld.ElGamals[i].Left.X = currentEncVotes.ElGamals[i].Left.X
+		assignment.EncVoteOld.ElGamals[i].Left.Y = currentEncVotes.ElGamals[i].Left.Y
+		assignment.EncVoteOld.ElGamals[i].Right.X = currentEncVotes.ElGamals[i].Right.X
+		assignment.EncVoteOld.ElGamals[i].Right.Y = currentEncVotes.ElGamals[i].Right.Y
+	}
+
+	witness, _ := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+	publicWitness, err := witness.Public()
+	if err != nil {
+		t.Fatalf("public witnes err %s", err)
+	}
+
+	// create proof
+	proof, err := plonk.Prove(ccs, pk, witness)
+	if err != nil {
+		t.Fatalf("prove error: %s", err)
+	}
+	t.Logf("proof: %+v", proof)
+
+	err = plonk.Verify(proof, vk, publicWitness)
+	if err != nil {
+		t.Fatalf("verify error: %s", err)
+	}
+
 }
